8b3c1b303fee64a3bc5b6fc05f41948c18103bb6
XiaoyuLi <xli083@connect.hkust-gz.edu.cn>
2025-07-10 09:38:45 +0800

    Add Qwen model compatibility and monkey patch for ZSMerge

---
+++ tests/test_qwen2_att.py
@@ -0,0 +1,18 @@
+import unittest
+from .test_utils import cls_init, gen_equal, cleanup_after_test
+
+@cleanup_after_test
+class TestQwen2Generator(unittest.TestCase):
+    model_name="Qwen/Qwen1.5-7B"
+    
+    @classmethod
+    def setUpClass(cls):
+        cls_init(cls)
+        return super().setUpClass()
+
+    def test_gen(self):
+        gen_equal(self)
+
+
+if __name__ == "__main__":
+    unittest.main()
